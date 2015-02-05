89e8681ef009b9f616612e0a6c6614a08ba01db1
D-Dev <D-Dev@users.noreply.github.com>
2015-02-05 20:31:28 +0000

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,31 @@
+<?php
+/*This logout.php script is based on Chapter 18 of
+PHP and MySQL for Dynamic Web Sites: Visual QuickPro Guide (4th Edition),
+and is used to carry-out the logout process for the site. */
+
+// This states a requirement to include the config file 
+// an optional alternative page header, then set the page title.
+require ('includes/config.inc.php'); 
+$page_title = 'Logout';
+include ('includes/header.html');
+
+// IF no first_name session variable exists, this will redirect the user
+if (!isset($_SESSION['first_name'])) 
+	{
+		$url = BASE_URL . 'index.html'; // This will define the URL
+		ob_end_clean(); // This will delete the buffer
+		header("Location: $url");
+		exit(); // This will exit the script		
+	} 
+else 
+	{ // ELSE, logout the user
+		$_SESSION = array(); // This will destroy the variables.
+		session_destroy(); // This will destroy the session itself.
+		setcookie (session_name(), '', time()-3600); // This will destroy the cookie.
+	}
+
+
+//echo '<h3>You are now logged out.</h3>';
+
+//include ('includes/footer.html');
+?>
