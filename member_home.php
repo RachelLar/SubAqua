551291aa28cd32fa596b0e5f64ec02439419750d
D-Dev <D-Dev@users.noreply.github.com>
2015-05-04 03:06:30 +0100

    Update member_home.php

--- member_home.php
+++ member_home.php
@@ -1,13 +1,14 @@
 <?php 
     // Include the configuration file:
     require ('includes/config.inc.php'); 
-    
+       
     // Start output buffering:
     ob_start();
 
     // Initialize a session:
     session_start();
-?>
+    
+    ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -104,14 +105,7 @@
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="member_home.php"><strong>Home</strong></a></li>
-                    <li><a href="member_page2.php"><strong>Content</strong></a></li>    
-                <!--<li><a href="index.html"><strong>Home</strong></a></li>
-                    <li><a href="about.html"><strong>About</strong></a></li>
-                    <li class="active"><a href="FAQs.html"><strong>FAQ's</strong></a></li>
-                    <li><a href="gallery.html"><strong>Gallery</strong></a></li>                
-                    <li><a href="members.html"><strong>Members</strong></a></li>
-                    <li><a href="schedule.html"><strong>Schedule</strong></a></li>                
-                    <li><a href="contact.php"><strong>Contact</strong></a></li>--> 
+                    <li><a href="member_page2.php"><strong>Content</strong></a></li>   
                 </ul>
                 <ul class="nav navbar-right navbar-nav ">
                     <li class="dropdown">           
@@ -130,7 +124,6 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-user"></i> <i class="glyphicon glyphicon-chevron-down"></i></a>
                             <ul class="dropdown-menu">
-                               <!-- <li><a href="#" data-toggle="modal" data-target="#basicModal"><i class="fa fa-cog"></i> Login</a></li>-->
                                <li><a href="change_mypassword.php" title="Change Your Password"><strong>Change Password</strong></a></li> 
                                <li class="divider"></li>
                                 <li><a href="logout.php"><i class="fa fa-sign-out"></i> Logout</a></li>
@@ -142,72 +135,14 @@
     </div>
 <!--End Navigation -->
 
-<!--Start Login Modal --> 
-    <div class="modal fade" id="basicModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
-                        <h4 class="modal-title" id="myModalLabel">Login</h4>
-                </div>
-                <div class="modal-body">
-                    <div class="row">
-                        <div class="col-xs-6">
-                            <div class="well">
-                                <form id="loginForm" method="POST" action="login.php" novalidate="novalidate">
-                                    <div class="form-group">
-                                        <label for="username" class="control-label">Username</label>
-                                        <input type="text" class="form-control" id="username" name="username" value="" required="" title="Please enter your Username" placeholder="example@gmail.com">
-                                        <span class="help-block"></span>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="password" class="control-label">Password</label>
-                                        <input type="password" class="form-control" id="password" name="password" value="" required="" title="Please enter your Password">
-                                        <span class="help-block"></span>
-                                    </div>
-                                    <div id="loginErrorMsg" class="alert alert-error hide">You have entered an incorrect Username or Password</div>
-                                        <div class="checkbox">
-                                            <label>
-                                                <input type="checkbox" name="remember" id="remember"> Remember login
-                                            </label>
-                                            <p class="help-block">(If using a private computer)</p>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary btn-block">Login</button>
-                                        <a href="forgot_password.html" class="btn btn-primary btn-block">Forgotten Password</a>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="col-xs-6">
-                            <div class="well"> 
-                                <p class="lead">Register <span class="text-primary">Now!</span></p>
-                                    <ul class="list-unstyled" style="line-height: 2">
-                                        <li><span class="fa fa-check text-success"></span> Dive Training</li>
-                                        <li><span class="fa fa-check text-success"></span> Open-water diving</li>
-                                        <li><span class="fa fa-check text-success"></span> With like-minded people</li>
-                                        <li><span class="fa fa-check text-success"></span> In a great atmosphere</li>
-                                        <li><span class="fa fa-check text-success"></span> And International trips<small>(...and more)</small></li>
-                                    </ul>
-                                <p><a href="members.html" class="btn btn-primary btn-block">Read more</a></p>
-                                <p><a href="register.php" class="btn btn-primary btn-block">Yes please, register now!</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-<!--End Login Modal -->
-
 <!-- Start Page Heading/Breadcrumbs -->
     <div style="padding:20px;">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 scheme2">
-                    <h1 class="page-header">Members Home
-                        
-                    </h1>
+                    <h1 class="page-header">Members Home</h1>
                     <ol class="breadcrumb  btn-primary" >
-                        <li><a href="index.html">Members</a>
+                        <li><a href="member_home.php">Members</a>
                         </li>
                         <li class="active" id="scheme2">Home</li>
                     </ol>
@@ -218,12 +153,21 @@
 <!-- End Page Heading/Breadcrumbs -->
 
 <?php
-    // Welcome the user (by name if they are logged in):
+   // Welcome the user by name if they are logged in
     echo '<h1 class="scheme2 text-center">Welcome';
-    if (isset($_SESSION['first_name'])) {
+    if (!isset($_SESSION['first_name'])) 
+        {//If the Session isn't set and therefore not logged in
+            ob_end_clean(); // This will delete the buffer
+            header("Location: index.html");// This returns the user to the Login page
+            exit(); // This will exit the script
+        }
+    else 
+        {
             echo ", {$_SESSION['first_name']}";
-    }
+        }
     echo '!</h1>';
+    
+    
 ?>     
        
 <!--Start Main Page Content -->
@@ -332,10 +276,10 @@
         <div class="well" >
             <div class="row">
                 <div class="col-md-8">
-                    <p style="font-size: 25px;"><strong>Don't forget to book your diving trips</strong></p>
+                    <p style="font-size: 25px;"><strong>Open the main website in a new window</strong></p>
                 </div>
                 <div class="col-md-4">
-                    <a class="btn btn-lg btn-primary btn-block" href="members.html" target="_blank">Find out more information NOW!</a>
+                    <a class="btn btn-lg btn-primary btn-block" href="index.html" target="_blank">Click Here!</a>
                 </div>
             </div>
         </div>
@@ -348,23 +292,7 @@
             <footer id="footer" class="text-center">      
                 <div class="row">                  
                     <div class="col-xs-8 col-sm-12">
-                       <!-- <ul class="list-inline">
-                            <li><a href="index.html"><strong>Home</strong></a></li>
-                            <li class="divider"></li>
-                            <li><a href="about.html"><strong>About</strong></a></li>
-                            <li class="divider"></li>
-                            <li><a href="FAQs.html"><strong>FAQ's</strong></a></li>
-                            <li class="divider"></li>
-                            <li><a href="gallery.html"><strong>Gallery</strong></a></li>
-                            <li class="divider"></li>
-                            <li><a href="members.html"><strong>Members</strong></a></li>
-                            <li class="divider"></li>
-                            <li><a href="schedule.html"><strong>Schedule</strong></a></li>
-                            <li class="divider"></li>
-                            <li><a href="contact.php"><strong>Contact</strong></a></li>
-                            <li class="divider"></li>
-                            <li><a href="sitemap.html"><strong>Site Map</strong></a></li>                      
-                        </ul> -->
+                       
                        	<?php 
                             // Display links based upon the login status:
                             if (isset($_SESSION['user_id'])) 
@@ -388,10 +316,15 @@
                                 if ($_SESSION['user_level'] == 1) 
                                     {
                                         echo '<ul class="list-inline">
-                                                <li><a href="admin_task.php" title="Admin Task"><strong>Admin Task</strong></a></li>
+                                                <li><a href="admin_home.php">Home</a></li>
                                                 <li class="divider"></li>
-                                                <li><a href="admin_task.php" title="Admin Task 2"><strong>Admin Task 2</strong></a></li>                
-
+                                                <li><a href="admin_view_members.php">View Members</a></li>
+                                                <li class="divider"></li>
+                                                <li><a href="admin_add_members_info.php">Add Members Admin Info</a></li>
+                                                <li class="divider"></li>
+                                                <li><a href="admin_upload_members_photo.php">Upload Member Photo</a></li>
+                                                <li class="divider"></li>
+                                                <li><a href="admin_add_members_payment.php">Add Members Payment Info</a></li>
                                             </ul>';
                                     }   
 
@@ -417,26 +350,13 @@
                                         </ul>';
                                 }
                     ?>
-                    </div>
-                    <!--<div class="col-xs-8 col-sm-12">
-                        <ul class="list-inline">
-                            <li><a href="termsofuse.html"><strong>Terms of Use</strong></a></li>
-                            <li class="divider"></li>
-                            <li><a href="index.html"><strong>&copy; Company 2014</strong></a></li>
-                            <li class="divider"></li>
-                            <li><a href="privacypolicy.html"><strong>Privacy Policy</strong></a></li>                
-
-                        </ul> 
-                    </div> -->                
-                
+                    </div>               
                 </div> 
             </footer>
         </div>
     </div>
 <!-- End Footer Content Section -->
          
-      
-
 <!-- Start Additional Scripting (for faster loading) -->  
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.js"><\/script>')</script>
@@ -445,7 +365,7 @@
 
     <script src="js/main.js"></script>
 
-<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. 
     <script>
         (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
         function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
@@ -453,7 +373,7 @@
         e.src='//www.google-analytics.com/analytics.js';
         r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
         ga('create','UA-XXXXX-X');ga('send','pageview');
-    </script>
+    </script>-->
     <script src="js/vendor/jquery-1.11.1.js"></script>
     <!-- easing plugin ( optional ) -->
     <script src="js/vendor/jquery-uitop/easing.js" type="text/javascript"></script>
@@ -475,8 +395,6 @@
 
             });
     </script>
-
-
 <!-- End Additional Scripting -->
 
 <!-- End Page -->
