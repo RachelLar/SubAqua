0a41f0659b92a3881d3bd3f05ea9284deb0a7bd5
D-Dev <D-Dev@users.noreply.github.com>
2015-05-04 02:57:55 +0100

    Update admin_add_members_info.php

--- admin_add_members_info.php
+++ admin_add_members_info.php
@@ -1,21 +1,28 @@
 <?php 
     // Include the configuration file:
     require ('includes/config.inc.php'); 
-    
-    
+       
     // Start output buffering:
     ob_start();
 
     // Initialize a session:
     session_start();
 
-   /*if ($_SESSION['user_level'] == 0) // If the user is not Admin, redirect to the Login page
+   if ($_SESSION['user_level'] == 0) // If the user is not Admin, redirect to the Login page
     {
 	ob_end_clean(); // This will delete the buffer
 	header("Location: index.html");
 	exit(); // This will exit the script
-    }*/
+    }
+    
+    if (!isset($_SESSION['first_name'])) 
+        {//If the Session isn't set and therefore not logged in
+            ob_end_clean(); // This will delete the buffer
+            header("Location: index.html");// This returns the user to the Login page
+            exit(); // This will exit the script
+        }
     ?>
+
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -108,21 +115,8 @@
                     <img src="img/TeamLogo.png" alt="Team Solent Sub-Aqua Club"/>
                 </div>  
             </div>
-          
-           
-            
+                     
             <div id="navbar" class="navbar-collapse collapse">
-                <!--<ul class="nav navbar-nav">
-                    <li class="active"><a href="member_home.php"><strong>Home</strong></a></li>
-                    <li><a href="member_page2.php"><strong></strong></a></li>    
-                <li><a href="index.html"><strong>Home</strong></a></li>
-                    <li><a href="about.html"><strong>About</strong></a></li>
-                    <li class="active"><a href="FAQs.html"><strong>FAQ's</strong></a></li>
-                    <li><a href="gallery.html"><strong>Gallery</strong></a></li>                
-                    <li><a href="members.html"><strong>Members</strong></a></li>
-                    <li><a href="schedule.html"><strong>Schedule</strong></a></li>                
-                    <li><a href="contact.php"><strong>Contact</strong></a></li>
-                </ul>--> 
                 <ul class="nav navbar-nav">
                 <li class="dropdown active">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">Admin Tasks<span class="caret"></span></a>
@@ -132,8 +126,7 @@
                     <li><a href="admin_add_members_info.php"><i class="fa fa-briefcase"></i> Add Members Admin Info</a></li>
                     <li><a href="admin_upload_members_photo.php"><i class="fa fa-briefcase"></i> Upload Member Photo</a></li>
                     <li><a href="admin_add_members_payment.php"><i class="fa fa-briefcase"></i> Add Members Payment Info</a></li>
-                    <!--<li class="divider"></li>-->
-                    
+                    <!--<li class="divider"></li>-->                    
                   </ul>
                 </li>
             </ul>
@@ -154,7 +147,6 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-user"></i> <i class="glyphicon glyphicon-chevron-down"></i></a>
                             <ul class="dropdown-menu">
-                               <!-- <li><a href="#" data-toggle="modal" data-target="#basicModal"><i class="fa fa-cog"></i> Login</a></li>-->
                                <li><a href="change_mypassword.php" title="Change Your Password"><strong>Change Password</strong></a></li> 
                                <li class="divider"></li>
                                 <li><a href="logout.php"><i class="fa fa-sign-out"></i> Logout</a></li>
@@ -182,26 +174,7 @@
         </div>
     </div>
 <!-- End Page Heading/Breadcrumbs -->
-
-<?php
-    // Welcome the user by name if they are logged in
-   /* echo '<h1>Welcome';
-    if (!isset($_SESSION['first_name'])) 
-        {//If the Session isn't set and therefore not logged in
-            ob_end_clean(); // This will delete the buffer
-            header("Location: index.html");// This returns the user to the Login page
-            exit(); // This will exit the script
-        }
-    else 
-        {
-            echo ", {$_SESSION['first_name']}";
-        }
-    echo '!</h1>';
     
-    */
-?>     
-
-
 <!--Start Main Page Content -->
 
 <div>
@@ -211,7 +184,6 @@
                     <div class="formbox formbox-area clearfix text-center">
                     <!--This will display the html registration form-->
                         <h1 class="textwhite">Add Member Admin Info</h1> 
-                        
                         <form class="form-group" role="form" action="add_member_info.php" method="post">
                             <div class="form-group form-group-sm">
                                 <input type="text" name="member_info_member_info_id" placeholder="Exact Member Info ID Number" class="form-control input-md  text-center" size="30" maxlength="30" value="<?php if (isset($trimmed['member_info_member_info_id'])) echo $trimmed['member_info_member_info_id']; ?>" />
@@ -237,9 +209,6 @@
             </div>
         </div>
     </div>
-
-
-
 <!--End Main Page Content -->
         
 <hr> <!--A line -->
@@ -249,10 +218,10 @@
         <div class="well" >
             <div class="row">
                 <div class="col-md-8">
-                    <p style="font-size: 25px;"><strong>Don't forget to book your diving trips</strong></p>
+                    <p style="font-size: 25px;"><strong>Open the main website in a new window</strong></p>
                 </div>
                 <div class="col-md-4">
-                    <a class="btn btn-lg btn-primary btn-block" href="members.html" target="_blank">Find out more information NOW!</a>
+                    <a class="btn btn-lg btn-primary btn-block" href="index.html" target="_blank">Click Here!</a>
                 </div>
             </div>
         </div>
@@ -264,36 +233,20 @@
         <div style="background-image: -webkit-linear-gradient(top,rgba(23, 22, 22, 0.45) 0,rgba(23, 22, 22, 0) 100%); background-color: #428bca; border-top: 1px solid black; border-bottom: 1px solid black;">   
             <footer id="footer" class="text-center">      
                 <div class="row">                  
-                    <div class="col-xs-8 col-sm-12">
-                       <!-- <ul class="list-inline">
-                            <li><a href="index.html"><strong>Home</strong></a></li>
-                            <li class="divider"></li>
-                            <li><a href="about.html"><strong>About</strong></a></li>
-                            <li class="divider"></li>
-                            <li><a href="FAQs.html"><strong>FAQ's</strong></a></li>
-                            <li class="divider"></li>
-                            <li><a href="gallery.html"><strong>Gallery</strong></a></li>
-                            <li class="divider"></li>
-                            <li><a href="members.html"><strong>Members</strong></a></li>
-                            <li class="divider"></li>
-                            <li><a href="schedule.html"><strong>Schedule</strong></a></li>
-                            <li class="divider"></li>
-                            <li><a href="contact.php"><strong>Contact</strong></a></li>
-                            <li class="divider"></li>
-                            <li><a href="sitemap.html"><strong>Site Map</strong></a></li>                      
-                        </ul> -->
+                    <div class="col-xs-8 col-sm-12 text-center">
+                       
                        	<?php 
                             // Display links based upon the login status:
                             if (isset($_SESSION['user_id'])) 
                                 {
-                                    echo '<ul class="list-inline">
+                                    echo '<ul class="list-inline text-center">
                                             <li><a href="logout.php" title="Logout"><strong>Logout</strong></a></li>
                                             <li class="divider"></li>
                                             <li><a href="change_mypassword.php" title="Change Your Password"><strong>Change Password</strong></a></li>               
 
                                         </ul>
 
-                                        <ul class="list-inline">
+                                        <ul class="list-inline text-center">
                                             <li><a href="termsofuse.html" target="_blank"><strong>Terms of Use</strong></a></li>
                                             <li class="divider"></li>
                                             <li><a href="index.html" target="_blank"><strong>&copy; Team Solent Sub-Aqua Club 2015</strong></a></li>
@@ -304,18 +257,23 @@
                                 // Add links if the user is an administrator:
                                 if ($_SESSION['user_level'] == 1) 
                                     {
-                                        echo '<ul class="list-inline">
-                                                <li><a href="admin_task.php" title="Admin Task"><strong>Admin Task</strong></a></li>
+                                        echo '<ul class="list-inline text-center">
+                                                <li><a href="admin_home.php"><strong>Home</strong></a></li>
                                                 <li class="divider"></li>
-                                                <li><a href="admin_task.php" title="Admin Task 2"><strong>Admin Task 2</strong></a></li>                
-
+                                                <li><a href="admin_view_members.php"><strong>View Members</strong></a></li>
+                                                <li class="divider"></li>
+                                                <li><a href="admin_add_members_info.php"><strong>Add Members Admin Info</strong></a></li>
+                                                <li class="divider"></li>
+                                                <li><a href="admin_upload_members_photo.php"><strong>Upload Member Photo</strong></a></li>
+                                                <li class="divider"></li>
+                                                <li><a href="admin_add_members_payment.php"><strong>Add Members Payment Info</strong></a></li>
                                             </ul>';
                                     }   
 
                                 } 
                             else 
                                 { //  Not logged in.
-                                    echo '<ul class="list-inline">
+                                    echo '<ul class="list-inline text-center">
                                             <li><a href="register.php" title="Register for the Site"><strong>Register</strong></a></li>
                                             <li class="divider"></li>
                                             <li><a href="#" data-toggle="modal" data-target="#basicModal"><strong>Login</strong></a></li>
@@ -324,7 +282,7 @@
 
                                         </ul>
 
-                                        <ul class="list-inline">
+                                        <ul class="list-inline text-center">
                                             <li><a href="termsofuse.html" target="_blank"><strong>Terms of Use</strong></a></li>
                                             <li class="divider"></li>
                                             <li><a href="index.html" target="_blank"><strong>&copy; Team Solent Sub-Aqua Club 2015</strong></a></li>
@@ -334,26 +292,13 @@
                                         </ul>';
                                 }
                     ?>
-                    </div>
-                    <!--<div class="col-xs-8 col-sm-12">
-                        <ul class="list-inline">
-                            <li><a href="termsofuse.html"><strong>Terms of Use</strong></a></li>
-                            <li class="divider"></li>
-                            <li><a href="index.html"><strong>&copy; Company 2014</strong></a></li>
-                            <li class="divider"></li>
-                            <li><a href="privacypolicy.html"><strong>Privacy Policy</strong></a></li>                
-
-                        </ul> 
-                    </div> -->                
-                
+                    </div>               
                 </div> 
             </footer>
         </div>
     </div>
 <!-- End Footer Content Section -->
-         
-      
-
+             
 <!-- Start Additional Scripting (for faster loading) -->  
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.js"><\/script>')</script>
@@ -362,7 +307,7 @@
 
     <script src="js/main.js"></script>
 
-<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. 
     <script>
         (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
         function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
@@ -370,7 +315,7 @@
         e.src='//www.google-analytics.com/analytics.js';
         r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
         ga('create','UA-XXXXX-X');ga('send','pageview');
-    </script>
+    </script>-->
     <script src="js/vendor/jquery-1.11.1.js"></script>
     <!-- easing plugin ( optional ) -->
     <script src="js/vendor/jquery-uitop/easing.js" type="text/javascript"></script>
@@ -392,8 +337,6 @@
 
             });
     </script>
-
-
 <!-- End Additional Scripting -->
 
 <!-- End Page -->
